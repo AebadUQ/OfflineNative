342ea5a2bf8dd29959ead818d63324c16f76345b
AebadUQ <aebadulquadir123@gmail.com>
2022-12-16 04:38:03 +0500

    "background"

--- App.js
+++ App.js
@@ -16,6 +16,53 @@ import VIForegroundService from '@voximplant/react-native-foreground-service';
 
 // import GetLocation from 'react-native-get-location'
 import { NetworkInfo } from "react-native-network-info";
+import BackgroundService from 'react-native-background-actions';
+const sleep = (time) => new Promise((resolve) => setTimeout(() => resolve(), time));
+const veryIntensiveTask = async (taskDataArguments) => {
+  // Example of an infinite loop task
+  const { delay } = taskDataArguments;
+  await new Promise( async (resolve) => {
+      for (let i = 0; BackgroundService.isRunning(); i++) {
+        
+       await  axios.post("https://test-rdgw45gi2q-oa.a.run.app/add_data", {
+          password:"sirsaulat",
+          data:{
+            name:"devname",
+            location:"ss",
+            ip_address:"ip",
+            timestamp:"finalObject"
+          }
+        })
+        .then((response) => {
+          console.log(response);
+        }
+        
+        )
+        .catch((err)=>{
+          console.log(err)
+        })
+        await BackgroundService.updateNotification({
+          taskDesc:'my count'+i
+        })
+       
+          await sleep(delay);
+      }
+  });
+};
+const options = {
+  taskName: 'Background',
+  taskTitle: 'ETask',
+  taskDesc: 'ExampleTask description',
+  taskIcon: {
+      name: 'ic_launcher',
+      type: 'mipmap',
+  },
+  color: '#ff00ff',
+  linkingURI: 'yourSchemeHere://chat/jane', // See Deep Linking for more info
+  parameters: {
+      delay: 5000,
+  },
+};
 
 
 const App= () => {
@@ -48,6 +95,15 @@ const startForegroundService=async()=> {
 }
 const stopForegroundService=async()=>{
   await VIForegroundService.getInstance().stopService();
+
+}
+const startBackgroundService=async()=>{
+  await BackgroundService.start(veryIntensiveTask, options);
+  await BackgroundService.updateNotification({taskDesc: 'New ExampleTask description'}); // Only Android, iOS will ignore this call
+  
+}
+const stopBackgroundService=async()=>{
+  await BackgroundService.stop();
 
 }
   // const [loc,setLoc]=useState('')
@@ -105,34 +161,33 @@ const stopForegroundService=async()=>{
 //     // console.warn(code, message);
 //   })
 //   console.log(loc?.latitude)
-//   axios.post("https://test-rdgw45gi2q-oa.a.run.app/add_data", {
-//     password:"sirsaulat",
-//     data:{
-//       name:devname,
-//       location:{
-//         latitude:loc?.latitude,
-//         longitude:loc?.longitude
-//       },
-//       ip_address:ip,
-//       timestamp:finalObject
-//     }
-//   })
-//   .then((response) => {
-//     console.log(response);
-//   });
+  // axios.post("https://test-rdgw45gi2q-oa.a.run.app/add_data", {
+  //   password:"sirsaulat",
+  //   data:{
+  //     name:devname,
+  //     location:{
+  //       latitude:loc?.latitude,
+  //       longitude:loc?.longitude
+  //     },
+  //     ip_address:ip,
+  //     timestamp:finalObject
+  //   }
+  // })
+  // .then((response) => {
+  //   console.log(response);
+  // });
 //   console.log(devname)
 // }
   return (
     <SafeAreaView>
-      <TouchableOpacity style={{borderWidth:1,margin:10,height:30}} onPress={()=>startForegroundService()}></TouchableOpacity>
-      <TouchableOpacity style={{borderWidth:1,margin:10}} onPress={()=>stopForegroundService()}><Text>2</Text></TouchableOpacity>
+      {/* <TouchableOpacity style={{borderWidth:1,margin:10,height:30}} onPress={()=>startForegroundService()}></TouchableOpacity>
+      <TouchableOpacity style={{borderWidth:1,margin:10}} onPress={()=>stopForegroundService()}><Text>2</Text></TouchableOpacity> */}
 
-      <TouchableOpacity style={{borderWidth:1,margin:10}}><Text>3</Text></TouchableOpacity>
+      <TouchableOpacity style={{borderWidth:1,margin:10}} onPress={()=>startBackgroundService()}><Text>3</Text></TouchableOpacity>
 
-      <TouchableOpacity style={{borderWidth:1,margin:10}}><Text>4</Text></TouchableOpacity>
+      <TouchableOpacity style={{borderWidth:1,margin:10}} onPress={()=>stopBackgroundService()}><Text>4</Text></TouchableOpacity>
 
         {/* <TouchableOpacity onPress={handle}><Text>Click</Text></TouchableOpacity> */}
-        <TouchableOpacity><Text>sadsa</Text></TouchableOpacity>
     </SafeAreaView>
   );
 };
